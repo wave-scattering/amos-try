32d0131adeffc4170bec1de8d8eb2737eefde720
Konstantin Ladutenko <kostyfisik@gmail.com>
2019-10-23 17:09:06 +0300

    initial

--- cylinder2mod/axspartclnew4.f
+++ cylinder2mod/axspartclnew4.f
@@ -89,6 +89,10 @@ C  Computation can be speed up if one sets YNCHECK=.FALSE..
 C  However, then the check of Gauss integrations
 C  convergence is not performed.
 C---------------------------------------------------------------------
+      use finer, only :  file_ini
+      use flap, only : command_line_interface
+      use penf, only : I4P
+
       use libcylinder
       implicit none
       integer LMX,LCS,ILCS,ikl,ieps,istep,ide,ndefp,itter
@@ -249,6 +253,28 @@ c Declarations:
       COMMON /TOSPHERECL/ ynperfconv
       common /CYLPAR/ rsnm, hlength
       common /EPSGG/ global_eps_r, global_eps_i
+C--------/---------/---------/---------/---------/---------/---------/--
+      !---------------------------------------------------------------
+      !     Read config
+      !---------------------------------------------------------------
+      character(999)                :: file_name  !< Name of INI file.
+      type(file_ini)                :: fini       !< INI file handler.
+      character(len=:), allocatable :: items(:,:) !< Items pairs.
+      integer                       :: error
+      character(len=:), allocatable :: string         !< String option.
+
+      call cli_parse
+      call fini%load(filename=file_name)
+      string = repeat(' ', 999)
+      call fini%get(section_name='general', option_name='particle_type',
+     &  val=string, error=error)
+      write(*,*) trim(string)
+!     call fini%get_items(items)
+!     do i=1,size(items,dim=1)
+!       print "(A)", trim(items(i,1))//' = '//trim(items(i,2))
+!     enddo
+      stop
+
 *
 * From here to spherec
 *---------------------------------------------------------------
@@ -1823,6 +1849,28 @@ C--------/---------/---------/---------/---------/---------/---------/--
  5000 FORMAT ('4X4 PHASE MATRIX')
 
       contains
+      subroutine cli_parse()
+        !< Build and parse test cli.
+        type(command_line_interface) :: cli  !< command line interface.
+        integer(I4P)                 :: error !< Error trapping flag.
+
+        call cli%init(progname='cylinder2mod',
+     &                 authors='',
+     &                 help='Usage: ',
+     &                 examples=["cylinder2mod -i config.ini"],
+     &                 epilog=new_line('a')//"all done")
+
+        call cli%add(switch='--ini',
+     &                switch_ab='-i',
+     &                help='name of ini file',
+     &                required=.false.,
+     &                def='default.ini',
+     &                act='store')
+
+        call cli%parse(error=error) ; if (error/=0) stop
+
+        call cli%get(switch='--ini', val=file_name)
+      endsubroutine cli_parse
 
 
 
