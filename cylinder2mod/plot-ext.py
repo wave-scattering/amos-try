e990d4701ac354fd0d0e36d6e7e135d318192649
Konstantin Ladutenko <kostyfisik@gmail.com>
2019-11-22 23:09:18 +0100

    update

--- cylinder2mod/plot-ext.py
+++ cylinder2mod/plot-ext.py
@@ -18,13 +18,33 @@ print('Negative', negval, 'of',len(data.flatten()))
 
 data = np.abs(data)
 
+import configparser
+config = configparser.ConfigParser()
+config.read('default.ini')
+x_min = float(config['beam']['x_min'])
+x_max = float(config['beam']['x_max'])
+
+rl_min = float(config['cylinder']['rl_min'])
+rl_max = float(config['cylinder']['rl_max'])
+
 plt.imshow(data,
            origin='lower',
-           cmap='jet',
+           cmap='hot',
            aspect='auto',
-           norm=LogNorm(
-               vmin = np.min(data)*1.0,
-               vmax = np.max(data)*1.0
-               )
+           vmin = np.mean(data)*0.1, vmax = np.mean(data)*4,
+           extent=(x_min, x_max, rl_min, rl_max),
+           # norm=LogNorm(
+           #     vmin = np.min(data)*1.0,
+           #     vmax = np.max(data)*1.0
+           #     )
            )
+plt.xlabel(r'$kr$')
+plt.ylabel(r'$r/L$')
+plt.colorbar()
+
+plt.figure()
+spectra = data[-1,:]
+spectra_x = np.linspace(x_min, x_max, len(spectra))
+plt.plot(spectra_x, spectra)
+plt.title(f'$r/L = ${rl_max}')
 plt.show()
