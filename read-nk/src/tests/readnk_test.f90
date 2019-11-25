ca93e2d1815cc55e825d9d754079ec661a5828dd
Konstantin Ladutenko <kostyfisik@gmail.com>
2019-11-25 20:44:20 +0100

    update

--- read-nk/src/tests/readnk_test.f90
+++ read-nk/src/tests/readnk_test.f90
@@ -8,9 +8,6 @@ program readnk_test
     real(dp), allocatable :: WLs(:), y(:)
     character(len=:), allocatable :: filename, prefix
     !=======================================================================
-    total_points = 101
-    from_WL = 0.3_dp
-    to_WL = 0.7_dp
     prefix = '../../../'
 !    filename = prefix//'nk-data/Au-Johnson.yml'
 !    filename = prefix//'nk-data/Au-Olmon-ev.yml'
@@ -20,22 +17,26 @@ program readnk_test
     filename = prefix//'nk-data/Si-Green-2008.yml'
 !    filename = prefix//'nk-data/TiO2-Bodurov.yml'
 !    filename = prefix//'nk-data/TiO2-Devore-o.yml'
-    call read_refractive_index_from_yaml(filename, ios, spectra)
 
+    !Get the whole spectra from the file
+    call read_refractive_index_from_yaml(filename, ios, spectra)
+    !Put it into txt file format
     open(unit=10, file="index.txt", iostat=ios)
     do i = 1, size(spectra,1),1
         write(10,*) spectra(i,:)
     end do
     close(10)
 
+    !Get interpolated spectra for a given spectral range
+    total_points = 101
+    from_WL = 0.3_dp
+    to_WL = 0.7_dp
     call get_refractive_index(filename, total_points, from_WL, to_WL, spectra_int)
 
+    !Put it into txt file format
     open(unit=10, file="index_int.txt", iostat=ios)
     do i = 1, size(spectra_int,1),1
         write(10,*) spectra_int(i,:)
     end do
     close(10)
-
-    write(*,*) "end"
-
 end program readnk_test
