6dc834f74e1478f7a26052ac0b5f05e4bf38cdad
Konstantin Ladutenko <kostyfisik@gmail.com>
2020-02-22 20:18:18 +0300

    add SMARTIES

--- cylinder2mod/plot-ext.py
+++ cylinder2mod/plot-ext.py
@@ -21,6 +21,7 @@ data = np.abs(data)
 import configparser
 config = configparser.ConfigParser()
 config.read('default.ini')
+# config.read('smarties_script_tutorial.ini')
 x_min = float(config['beam']['x_min'])
 x_max = float(config['beam']['x_max'])
 
@@ -29,33 +30,33 @@ rl_max = float(config['cylinder']['rl_max'])
 plt.figure()
 spectra = data[0,:]
 spectra_x = np.linspace(x_min, x_max, len(spectra))
-plt.plot(spectra_x, spectra)
+plt.plot(2*np.pi*100/spectra_x, spectra)
 spectra = data[-1,:]
-plt.plot(spectra_x, spectra)
+plt.plot(2*np.pi*100/spectra_x, spectra)
 if data.shape[0] > 7:
     spectra = data[7,:]
-    plt.plot(spectra_x, spectra)
+    plt.plot(2*np.pi*100/spectra_x, spectra)
 # plt.title(f'$r/L = ${rl_max}')
 plt.tight_layout()
 # plt.show()
 plt.savefig('spectra.pdf')
 
-plt.figure()
-plt.imshow(data,
-           origin='lower',
-           cmap='hot',
-           aspect='auto',
-           # vmin = np.mean(data)*0.1, vmax = np.mean(data)*4,
-           # extent=(x_min/2/np.pi/50*300, x_max/2/np.pi/50*300, rl_min, rl_max),
-           extent=(x_min, x_max, rl_min, rl_max),
-           norm=LogNorm(
-               vmin = np.min(data)*1.0,
-               vmax = np.max(data)*1.0
-               )
-           )
-plt.xlabel(r'$kr$')
-plt.ylabel(r'$r/L$')
-plt.tight_layout()
-# plt.colorbar()
-# plt.show()
-plt.savefig('map.pdf')
+# plt.figure()
+# plt.imshow(data,
+#            origin='lower',
+#            cmap='hot',
+#            aspect='auto',
+#            # vmin = np.mean(data)*0.1, vmax = np.mean(data)*4,
+#            # extent=(x_min/2/np.pi/50*300, x_max/2/np.pi/50*300, rl_min, rl_max),
+#            extent=(x_min, x_max, rl_min, rl_max),
+#            norm=LogNorm(
+#                vmin = np.min(data)*1.0,
+#                vmax = np.max(data)*1.0
+#                )
+#            )
+# plt.xlabel(r'$kr$')
+# plt.ylabel(r'$r/L$')
+# plt.tight_layout()
+# plt.savefig('map.pdf')
+
+plt.show()
